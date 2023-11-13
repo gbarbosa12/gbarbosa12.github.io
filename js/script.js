3bcaf29cefba86925a84ee474006ceae587c9759
Gabriel Amaral <gabriel2012amaral@gmail.com>
2023-11-13 19:32:47 -0300

    Adicionando JS na parte de projetos

--- js/script.js
+++ js/script.js
@@ -1,7 +1,26 @@
-// function initIntroducao() {
-//   const introducao = document.querySelector(".introducao");
+const projetosLista = document.querySelectorAll("[data-tab='list'] li");
+const conteudoLista = document.querySelectorAll("[data-tab='content'] section");
 
-//   introducao.classList.add(".ativo");
-// }
+conteudoLista[0].classList.add("ativo");
+projetosLista[0].classList.add("ativo");
 
-// initIntroducao();
+function handleClick(index) {
+  projetosLista.forEach((section) => {
+    section.classList.remove("ativo");
+  });
+
+  conteudoLista.forEach((section) => {
+    section.classList.remove("ativo");
+  });
+
+  projetosLista[index].classList.add("ativo");
+
+  const direcao = conteudoLista[index].dataset.anime;
+  conteudoLista[index].classList.add("ativo", direcao);
+}
+
+projetosLista.forEach((projeto, index) => {
+  projeto.addEventListener("click", function () {
+    handleClick(index);
+  });
+});
